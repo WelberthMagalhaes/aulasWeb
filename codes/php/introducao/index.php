01f5bd340f3b4f85a91e204e51bf2e62d4aabfe4
WelberthMagalhaes <welberthheider@gmail.com>
2019-10-29 22:05:13 -0300

    aula29-10

---
+++ codes/php/introducao/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP: Introdução</title>
+  </head>
+  <body>
+
+    <?php echo "Hello Word!"; ?>
+
+
+  </body>
+</html>
---
+++ codes/php/introducao/validar.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Menu Principal</title>
+  </head>
+  <body>
+    <p>Seja bem-vindo(a), <?php echo htmlspecialchars($_POST['usuario'];)?>!</p>
+  </body>
+</html>
